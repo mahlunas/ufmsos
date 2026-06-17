2212af53778a2f824085eb7a163950520ffb9580
luna <marcelle@exiti.com.br>
2026-06-17 16:08:21 -0300

    retirada da aba candidaturas, será substituida pela aba de curriculo

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -4,7 +4,6 @@ import Login from "../pages/Login.tsx";
 import User from "../pages/User.tsx";
 import Estagio from "../pages/Estagio.tsx";
 import Saude from "../pages/Saude.tsx";
-import Candidaturas from "../pages/Candidaturas.tsx";
 import Financas from "../pages/Financas.tsx";
 import Estudos from "../pages/Estudos.tsx";
 import MainLayout from "../pages/MainLayout.tsx";
@@ -31,7 +30,6 @@ export default function AppRoutes(){
                         <Route path="/user" element={<User/>}/>
                         <Route path="/estagio" element={<Estagio/>}/>
                         <Route path="/saude" element={<Saude/>}/>
-                        <Route path="/candidaturas" element={<Candidaturas/>}/>
                         <Route path="/financas" element={<Financas/>}/>
                         <Route path="/estudos" element={<Estudos/>}/>
                     </Route>
